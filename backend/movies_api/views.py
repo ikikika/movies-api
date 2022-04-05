23a1d1b11ff9068bcbc02c2ee511d7910e5d5289
ikikika <iki.jito@gmail.com>
2022-04-06 01:28:20 +0800

    create viewset for movies

--- backend/movies_api/views.py
+++ backend/movies_api/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework import filters
 
-# Create your views here.
+from movies_api import serializers
+from movies_api import models
+
+
+class MoviesViewSet(viewsets.ModelViewSet):  # ModelViewSet need queryset
+    """Handle querying profiles"""
+    serializer_class = serializers.MovieSerializer
+    queryset = models.Movie.objects.all()
+    filter_backends = (filters.SearchFilter,)
+    search_fields = ('title', 'releaseYear',)

fe8d3ea9d6bb5a9d722db290719f4bc52d33a1a4
ikikika <iki.jito@gmail.com>
2022-04-06 01:25:53 +0800

    create model for movies

--- backend/movies_api/models.py
+++ backend/movies_api/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
-# Create your models here.
+
+class Movie(models.Model):
+    """Database model for movies"""
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    programType = models.CharField(max_length=255)
+    releaseYear = models.PositiveIntegerField()
+    imagesUrl = models.CharField(max_length=255)
+    imagesWidth = models.PositiveIntegerField()
+    imagesHeight = models.PositiveIntegerField()
+
+    def get_title(self):
+        """Retrieve title of movie"""
+        return self.title
+
+    def get_programType(self):
+        """Retrieve programType of movie"""
+        return self.programType
+
+    def get_releaseYear(self):
+        """Retrieve releaseYear of movie"""
+        return self.releaseYear
--- readme.md
+++ readme.md
@@ -7,3 +7,5 @@ cd into backend
 `cd /vagrant`
 `pip install -r requirements.txt`
 `python manage.py runserver 0.0.0.0:8000`
+`python manage.py migrate`
+`python manage.py createsuperuser`
